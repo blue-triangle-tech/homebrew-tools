006974c2b0dab1c84321b1242e8bf58f9306d6dc
AshokKSingh1 <v-ashok.singh@bluetriangle.com>
2026-06-29 15:32:41 +0530

    Update bttinstrumentor.rb
    Updated sha

--- Formula/bttinstrumentor.rb
+++ Formula/bttinstrumentor.rb
@@ -3,8 +3,8 @@ class Bttinstrumentor < Formula
   homepage "https://www.bluetriangle.com"
   version "1.0.0"
 
-  url "https://github.com/ashok1404/BTTInstrumentor/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
-  sha256 "3ae9b0c19a60100880f826ce846b0da67bf49bb10dae371b178e33eec437c5c9"
+  url "https://github.com/blue-triangle-tech/BTTInstrumentor/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
+  sha256 "8773ec669af8951877a97dae39cf9d21eee31f8962b4728b2895035f70d377a7"
 
   def install
     bin.install "BTTInstrumentor"
