f2cd74c9003a88a18583b4a199bbfba4ba135a8b
AshokKSingh1 <v-ashok.singh@bluetriangle.com>
2026-06-30 21:21:09 +0530

    Initial Update (#1)
    * Updated  README.md
    
    * pointing to updated formula

--- Formula/bttinstrumentor.rb
+++ Formula/bttinstrumentor.rb
@@ -1,10 +1,10 @@
 class Bttinstrumentor < Formula
   desc "BlueTriangle SwiftUI automatic screen tracking instrumentor"
-  homepage "https://www.bluetriangle.com"
+  homepage "https://help.bluetriangle.com/hc/en-us/articles/52918697353875-iOS-SwiftUI-SDK-Instrumentation-Automated-Screen-Tracking"
   version "1.0.0"
 
-  url "https://github.com/blue-triangle-tech/BTTInstrumentor/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
-  sha256 "8773ec669af8951877a97dae39cf9d21eee31f8962b4728b2895035f70d377a7"
+  url "https://github.com/blue-triangle-tech/BTTInstrumentor/releases/download/1.0.0/BTTInstrumentor"
+  sha256 "8f7f62f7d625a3173d7ccd4965f99a7026ded5e56a08c3dc9e956de0510c589f"
 
   def install
     bin.install "BTTInstrumentor"
---
+++ README.md
@@ -0,0 +1,4 @@
+# Homebrew tap for BTTInstrumentor
+
+For more information on [instrumentor](https://github.com/blue-triangle-tech/BTTInstrumentor), visit the [**Official Help Doc**](https://help.bluetriangle.com/hc/en-us/articles/52918697353875-iOS-SwiftUI-SDK-Instrumentation-Automated-Screen-Tracking)
+
